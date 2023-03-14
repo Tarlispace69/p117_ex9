4f099e5032b9507b1b342740674cf518b616dec3
user <user@ZC415014>
2023-03-14 08:55:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace p117_ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            int i = 0;
+            
+            for (i=5; i <= 100; i+=5)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
